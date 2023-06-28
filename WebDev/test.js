10b582c4a1cae17824f0a8e1008d3cf39ff313aa
bohra <1rn20cs010.adityabohra@gmail.com>
2023-06-28 09:45:43 +0530

    new files

---
+++ WebDev/test.js
@@ -0,0 +1,6 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+function myFunction() {
+    document.getElementById("demo").innerHTML = z;
+}
---
+++ WebDev/test1.js
@@ -0,0 +1,6 @@
+var x = 10;
+var y = 10;
+var z = x + y;
+function myFunction() {
+    document.getElementById("demo").innerHTML = z;
+}
